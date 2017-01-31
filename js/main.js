96135ff45b3bc62c2ac62491b3cce096c7fe485c
Abby Schmiedt <abbyschmiedt@pcp066727pcs.wireless.calpoly.edu>
2017-01-31 15:01:27 -0800

    Added contact form functionality.

--- js/main.js
+++ js/main.js
@@ -24,3 +24,26 @@ jQuery(document).ready(function($){
 		});
 	}
 });
+
+    var form = document.getElementsByTagName('form')[0];
+    form.addEventListener('submit', contact, false);
+    function contact(e) {
+        e.preventDefault();
+
+        var target = e.target || e.srcElement;
+        var i = 0;
+        var message = '';
+
+        for(i=0; i < target.length; i++) {
+            if (target[i].type == 'text' || target[i].type == 'textarea') {
+                message += target[i].name + ':' + target[i].value + "\n";
+            }
+        }
+        target.elements["body"].value = message;
+
+        this.submit();
+
+    }
+
+
+
